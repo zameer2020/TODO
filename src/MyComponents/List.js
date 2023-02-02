f73620c4457067b2329cf2fe4f19f2444f9f672e
Sikindar Mirza <sikindar247@gmail.com>
2023-02-02 12:11:41 +0530

    Update List.js

--- src/MyComponents/List.js
+++ src/MyComponents/List.js
@@ -1,14 +1,38 @@
 
-function List({ users, handleEditUser, handleDeleteUser }) {
+function List({ users, handleEditUser, handleDeleteUser, showDeletedUsers }) {
+
+  const fetchUsers = () => {
+    const currentUsers = [...users];
+    if(showDeletedUsers) {
+      let deletedUsers = currentUsers.filter((user) => {
+         if(user.isDeleted) {
+          return user;
+         }
+      })
+      return deletedUsers
+    } else {
+      let notDeltedUsers = currentUsers.filter((user) => {
+        if(!user.isDeleted) {
+          return user;
+        }
+      })
+      return notDeltedUsers;
+    }
+  }
+  
   return (
     <ul>
-      {users.map((user, index) => (
-        <li key={index}>
-          {user}
-          <button onClick={() => handleEditUser(index)}>Edit</button>
-          <button onClick={() => handleDeleteUser(index)}>Delete</button>
-        </li>
-      ))}
+      {fetchUsers().map((user, index) => {
+     
+          return(
+          <li key={index}>
+            {user.name}
+            <button onClick={() => handleEditUser(user.id)}>Edit</button>
+            <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+            </li>
+          )
+       
+      })}
     </ul>
   );
 }
