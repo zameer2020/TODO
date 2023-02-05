07012d6921786186d339883b1a6277d450b55601
zameer2020 <zameerbashamirza2020@gmail.com>
2023-02-06 01:21:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
-import AddUser from "./MyComponents/AddUser";
-import List from "./MyComponents/List"
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [users, setUsers] = useState([]);
+const URL = "https://api.github.com/users";
+
+
+const Final = () => {
+  const [usersData, setUsersData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState({ status: false, msg: "" });
+
+  const fetchUsersData = async (apiURL) => {
+    setLoading(true);
+    setIsError({ status: false, msg: "" });
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      setUsersData(data);
+      setLoading(false);
+      setIsError({ status: false, msg: "" });
+      if (response.status === 404) {
+        throw new Error("data not found");
+      }
+    } catch (error) {
+      setLoading(false);
+      setIsError({
+        status: true,
+        msg: error.message || "something went wrong, pls try again!",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUsersData(URL);
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    );
+  }
+
+  if (isError?.status) {
+    return (
+      <div>
+        <h3 style={{ color: "red" }}>{isError?.msg}</h3>
+      </div>
+    );
+  }
 
   return (
-    <div className="App">
-      <AddUser users={users} setUsers={setUsers} />
-      <List users={users} />
+    <div>
+      <h1>Assignment</h1>
+      <ul>
+        {usersData.map((eachUser,index) => {
+          const { login,avatar_url,followers_url
+        } = eachUser;
+
+        const showFollowers=()=>{
+
+          return  {followers_url}
+        }
+          return (
+            <div>
+                <li>{login}</li>
+              <li>{avatar_url}</li>
+              <button onClick={showFollowers}> show followers</button>
+             
+              
+              </div>
+        
+
+
+            
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default Final;
